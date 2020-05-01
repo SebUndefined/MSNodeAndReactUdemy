7165acb8225c088d457c4ffeb46e06992e725200
Sébastien Tachier <sebastien.tachier@sebundefined.fr>
2020-05-01 17:32:32 +0200

    Sync events betwenn services

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async (postId) => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData(postId);
-  }, [postId]);
-
+export default ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if(comment.status === 'approved'){
+      content = comment.content
+    }
+    if(comment.status === 'pending') {
+      content = 'This Comment is awaiting for moderation'
+    }
+    if(comment.status === 'rejected') {
+      content = 'This Comment has been rejected'
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
